0ed58a77dfc466d6651adfbf89e326388c447407
prgrm274 <programmer270487@gmail.com>
2021-08-06 07:47:26 +0700

    password bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:project/presentation/pages/custom/my_clipper.dart';
-import 'package:project/presentation/pages/custom/my_clipper_base.dart';
+import 'package:project/presentation/my_app.dart';
+import 'package:project/presentation/pages/custom/row_circles.dart';
 import 'package:project/presentation/widgets/email1.dart';
 
 void main() {
   runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Screen 1'),
-          backgroundColor: Colors.indigo,
-        ),
-        body: Email1(),
-      ),
-    );
-  }
 }
